10a59cc45e50559e3cb659fcb31648417fab512c
Rakhi <Rakhi>
2017-04-28 02:32:23 +0600

    sign up integration

--- do_register.php
+++ do_register.php
@@ -13,15 +13,29 @@ $email = $_POST["email"];
 $name = $_POST["name"];
 $password = $_POST["password"];
 $phone = $_POST["phone"];
-$phone_privacy=1;
+$phone_privacy=$_POST["phone_privacy"];
 $photo = "photo";
-$gender="female";
+$gender=$_POST["gender"];
+$department = $_POST["department"];
+$biography = $_POST["biography"];
 
 
-$sql = "INSERT INTO user_info (username, email, name, password, gender,phone, phone_privacy,photo,verified) VALUES ('$username', '$email','$name', '$password','$gender', '$phone', '$phone_privacy','$photo',0)";
+$target_dir = "uploads/";
+$target_file = $target_dir.$_FILES["fileToUpload"]["name"];
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+$target_file=$target_dir.$username.".".$imageFileType;
+if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+
+
+$sql = "INSERT INTO user_info (username, email, name, password, gender,phone, phone_privacy,department,biography,verified) VALUES ('$username', '$email','$name', '$password','$gender', '$phone', '$phone_privacy','$department','$biography',0)";
 
 if ($conn->query($sql) == TRUE) {
-   echo"Registration is complete! ^_^";
+    header( "Location: registered.php" );
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
