8007de933099a21504b1519d89ccea96f639e07c
Rakhi <Rakhi>
2017-05-08 02:05:05 +0600

    almost complete

--- config.php
+++ config.php
@@ -5,9 +5,9 @@ $username_database="root";
 $password_database="";
 $dbname = "tutors_of_du";
 
-/*$username_database = "tutorsfromdu";
+$username_database = "tutorsfromdu";
 $password_database = "tutors0312";
-$dbname = "tutorsfromdu";*/
+$dbname = "tutorsfromdu";
 
 $conn = new mysqli($servername, $username_database, $password_database, $dbname);
 if ($conn->connect_error) {
